2e7ce06a8df78837977182a769b8fb879a7950d8
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-02-16 13:44:54 -0700

    Make a time loop based on "Fix Your Timestep"

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { default as canvas } from "./canvas.js";
+import canvas from "./canvas.js";
 
 var memory;
 
@@ -21,4 +21,40 @@ fetch("snake-game.wasm")
     .then(instance => {
         memory = instance.exports.memory;
         instance.exports.onInit();
+
+        // Timestep based on the Gaffer on Games post, "Fix Your Timestep"
+        //    https://www.gafferongames.com/post/fix_your_timestep/
+        const MAX_DELTA = 0.25;
+        const TICK_DELTA = (16 / 1000);
+        let prevTime = new Date().getTime();
+        let tickTime = 0.0;
+        let accumulator = 0.0;
+
+        function step() {
+            const newTime = new Date().getTime();
+            const delta = (newTime - prevTime) / 1000; // Delta in seconds
+            if (delta > MAX_DELTA) {
+                delta = MAX_DELTA; // Try to avoid spiral of death when lag hits
+            }
+            prevTime = newTime;
+
+            accumulator += delta;
+
+            while ( accumulator >= TICK_DELTA ) {
+                instance.exports.update(tickTime, TICK_DELTA);
+                accumulator -= TICK_DELTA;
+                tickTime += TICK_DELTA;
+            }
+
+            // Where the render is between two timesteps.
+            // If we are halfway between frames (based on what's in the accumulator)
+            // then alpha will be equal to 0.5
+            const alpha = accumulator / TICK_DELTA;
+
+            instance.exports.render(alpha);
+            window.requestAnimationFrame(step);
+
+        }
+
+        window.requestAnimationFrame(step);
     });
--- src/main_web.zig
+++ src/main_web.zig
@@ -1,8 +1,21 @@
 const platform = @import("platform.zig");
 
+var x: i32 = 10;
+
 export fn onInit() void {
     const message = "Hello, world!";
     platform.consoleLogS(message, message.len);
+}
+
+export fn update(current_time: f64, delta: f64) void {
+    x += @floatToInt(i32, 640 * delta);
+}
+
+export fn render(alpha: f64) void {
+    const w = platform.getScreenW();
+    const h = platform.getScreenW();
+    platform.clearRect(0, 0, w, h);
+
     platform.setFillStyle(100, 0, 0);
-    platform.fillRect(50, 50, 50, 50);
+    platform.fillRect(x, 50, 50, 50);
 }
