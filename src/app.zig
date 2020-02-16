8a146a333d61dbd1a22fb0dd8ecb62b616936546
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-02-16 14:14:35 -0700

    Separate main function and app logic

---
+++ src/app.zig
@@ -0,0 +1,19 @@
+const platform = @import("platform.zig");
+
+var x: i32 = 10;
+
+pub fn onInit() void {
+    platform.log("Hello, world!");
+}
+
+pub fn update(current_time: f64, delta: f64) void {
+    x += @floatToInt(i32, 640 * delta);
+}
+
+pub fn render(alpha: f64) void {
+    const screen_size = platform.getScreenSize();
+    platform.clearRect(0, 0, screen_size.x, screen_size.y);
+
+    platform.setFillStyle(100, 0, 0);
+    platform.fillRect(x, 50, 50, 50);
+}
--- src/main_web.zig
+++ src/main_web.zig
@@ -1,19 +1,13 @@
-const platform = @import("platform.zig");
-
-var x: i32 = 10;
+const app = @import("app.zig");
 
 export fn onInit() void {
-    platform.log("Hello, world!");
+    app.onInit();
 }
 
 export fn update(current_time: f64, delta: f64) void {
-    x += @floatToInt(i32, 640 * delta);
+    app.update(current_time, delta);
 }
 
 export fn render(alpha: f64) void {
-    const screen_size = platform.getScreenSize();
-    platform.clearRect(0, 0, screen_size.x, screen_size.y);
-
-    platform.setFillStyle(100, 0, 0);
-    platform.fillRect(x, 50, 50, 50);
+    app.render(alpha);
 }
