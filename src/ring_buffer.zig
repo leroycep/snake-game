dd72b34be3f93437c03a8d3f0a2f09c2189f41be
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-02-29 00:36:36 -0700

    Make RingBuffer generic

--- src/ring_buffer.zig
+++ src/ring_buffer.zig
@@ -1,56 +1,74 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
-const RingBuffer = struct {
-    buffer: []f32,
-    head: usize,
-    tail: usize,
+pub fn RingBuffer(comptime T: type) type {
+    return struct {
+        buffer: []T,
+        head: usize,
+        tail: usize,
 
-    pub fn init(buffer: []f32) @This() {
-        return .{
-            .buffer = buffer,
-            .head = 0,
-            .tail = 0,
-        };
-    }
-
-    pub fn push(self: *@This(), data: f32) !void {
-        var next = self.head + 1;
-
-        if (next >= self.buffer.len) {
-            next = 0;
+        pub fn init(buffer: []T) @This() {
+            return .{
+                .buffer = buffer,
+                .head = 0,
+                .tail = 0,
+            };
         }
 
-        if (next == self.tail) {
-            return error.BufferOverflow;
+        pub fn push(self: *@This(), data: T) !void {
+            var next = self.head + 1;
+
+            if (next >= self.buffer.len) {
+                next = 0;
+            }
+
+            if (next == self.tail) {
+                return error.BufferOverflow;
+            }
+
+            self.buffer[self.head] = data;
+            self.head = next;
         }
 
-        self.buffer[self.head] = data;
-        self.head = next;
-    }
+        pub fn pop(self: *@This()) ?T {
+            if (self.head == self.tail) {
+                return null;
+            }
 
-    pub fn pop(self: *@This()) ?f32 {
-        if (self.head == self.tail) {
-            return null;
+            var next = self.tail + 1;
+            if (next >= self.buffer.len) {
+                next = 0;
+            }
+
+            defer self.tail = next;
+            return self.buffer[self.tail];
         }
 
-        var next = self.tail + 1;
-        if (next >= self.buffer.len) {
-            next = 0;
+        pub fn len(self: *@This()) usize {
+            if (self.head == self.tail) {
+                return 0;
+            } else if (self.head > self.tail) {
+                return self.head - self.tail;
+            } else {
+                return self.buffer.len - self.tail + self.head;
+            }
         }
 
-        defer self.tail = next;
-        return self.buffer[self.tail];
-    }
-};
+        pub fn capacity(self: *@This()) usize {
+            return self.buffer.len - 1;
+        }
+    };
+}
 
 test "Ringbuffer simple test" {
-    var buf = [_]f32{0} ** 10;
-    var ring = RingBuffer.init(buf[0..]);
+    var buf = [_]i32{0} ** 10;
+    var ring = RingBuffer(i32).init(buf[0..]);
 
     try ring.push(1);
     try ring.push(2);
     try ring.push(3);
+
+    assert(ring.head == 3);
 
     assert(ring.pop().? == 1);
     assert(ring.pop().? == 2);
