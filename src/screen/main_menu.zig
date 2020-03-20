a691425ffe9b9168387e6257597caf1f5a40aeed
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-19 23:32:22 -0600

    Show different text when hovering on other buttons

--- src/screen/main_menu.zig
+++ src/screen/main_menu.zig
@@ -6,12 +6,55 @@ const Renderer = @import("../renderer.zig").Renderer;
 const ComponentRenderer = platform.components.ComponentRenderer;
 const Component = platform.components.Component;
 const Layout = platform.components.Layout;
+const Events = platform.components.Events;
+const utils = @import("../utils.zig");
+const Option = utils.Option;
 
-const NORMAL_PLAY_PRESSED = 1;
-const HOVER_NORMAL_PLAY = 2;
+const NORMAL_PLAY_CLICK = 1;
+const NORMAL_PLAY_HOVER = 2;
+const NORMAL_PLAY_EVENTS = Events{
+    .click = NORMAL_PLAY_CLICK,
+    .hover = NORMAL_PLAY_HOVER,
+};
+const CASUAL_PLAY_CLICK = 3;
+const CASUAL_PLAY_HOVER = 4;
+const CASUAL_PLAY_EVENTS = Events{
+    .click = CASUAL_PLAY_CLICK,
+    .hover = CASUAL_PLAY_HOVER,
+};
+const HIGHSCORES_CLICK = 5;
+const HIGHSCORES_HOVER = 6;
+const HIGHSCORES_EVENTS = Events{
+    .click = HIGHSCORES_CLICK,
+    .hover = HIGHSCORES_HOVER,
+};
 
 const Desc = enum {
     NormalPlay,
+    CasualPlay,
+    Highscores,
+
+    pub fn getText(self: @This()) []const u8 {
+        return switch (self) {
+            .NormalPlay => {
+                return
+                    \\ Eat as much fruit as you can, but make sure not to hit your tail!
+                ;
+            },
+            .CasualPlay => {
+                return
+                    \\ Don't like worrying about your score, but love to see the snake
+                    \\ get longer? In this mode you'll pass harmlessly over yourself,
+                    \\ and no score will be recorded.
+                ;
+            },
+            .Highscores => {
+                return
+                    \\ See the highscores (scores are local to this machine).
+                ;
+            },
+        };
+    }
 };
 
 pub const MainMenu = struct {
@@ -21,13 +64,15 @@ pub const MainMenu = struct {
     dirty: bool = true,
     component_renderer: ComponentRenderer,
     play_pressed: bool = false,
-    desc: ?Desc = null,
+    desc: Option(Desc) = Option(Desc){ .None = {} },
+    dependencies: utils.Dependencies(Option(Desc)),
 
     pub fn init(alloc: *std.mem.Allocator) !*@This() {
         const self = try alloc.create(@This());
         self.* = .{
             .alloc = alloc,
             .component_renderer = try ComponentRenderer.init(alloc),
+            .dependencies = utils.Dependencies(Option(Desc)).init(),
             .screen = .{
                 .onEventFn = onEvent,
                 .updateFn = update,
@@ -49,11 +94,10 @@ pub const MainMenu = struct {
                 else => {},
             },
             .Custom => |eventId| switch (eventId) {
-                NORMAL_PLAY_PRESSED => self.play_pressed = true,
-                HOVER_NORMAL_PLAY => {
-                    self.desc = .NormalPlay;
-                    self.dirty = true;
-                },
+                NORMAL_PLAY_CLICK => self.play_pressed = true,
+                NORMAL_PLAY_HOVER => self.desc = .{ .Some = .NormalPlay },
+                CASUAL_PLAY_HOVER => self.desc = .{ .Some = .CasualPlay },
+                HIGHSCORES_HOVER => self.desc = .{ .Some = .Highscores },
                 else => platform.warn("Unknown event id: {}\n", .{eventId}),
             },
             else => {},
@@ -74,23 +118,20 @@ pub const MainMenu = struct {
     pub fn render(screenPtr: *Screen, renderer: *Renderer, alpha: f64) void {
         const self = @fieldParentPtr(@This(), "screen", screenPtr);
 
+        if (!self.dependencies.is_changed(self.desc)) return;
+
         const text = platform.components.text;
         const box = platform.components.box;
         const button = platform.components.button;
 
-        if (!self.dirty) return;
-
-        var description: []const u8 = undefined;
-        if (self.desc) |desc| {
-            description = switch (desc) {
-                .NormalPlay => "Eat as much fruit as you can, but make sure not to hit your tail!",
-            };
-        } else {
-            description = "";
-        }
+        const description = switch (self.desc) {
+            .Some => |desc| desc.getText(),
+            .None => "",
+        };
 
         const grid = Layout.grid(.{
             .columns = &[_]u32{ 1, 1 },
+            .rows = &[_]u32{ 1, 1 },
             .areas = &[_][]usize{
                 &[_]usize{ 0, 0 },
                 &[_]usize{ 2, 1 },
@@ -105,14 +146,14 @@ pub const MainMenu = struct {
             text("Snake Game"),
             text(description),
             box(button_grid, &[_]Component{
-                button("Normal Play", .{ .click = NORMAL_PLAY_PRESSED, .hover = HOVER_NORMAL_PLAY }),
-                button("Casual Play", .{}),
-                button("Highscores", .{}),
+                button("Normal Play", NORMAL_PLAY_EVENTS),
+                button("Casual Play", CASUAL_PLAY_EVENTS),
+                button("Highscores", HIGHSCORES_EVENTS),
             }),
         });
         self.component_renderer.render(&components) catch unreachable;
 
-        self.dirty = false;
+        self.dependencies.update(self.desc);
     }
 
     pub fn stop(screenPtr: *Screen) void {
